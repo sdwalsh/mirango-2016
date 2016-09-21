d36da8f0ebf634fafc8eac23bab4b0c947540590
Sean Walsh <sdwalsh@mirango.io>
2016-09-21 07:03:32 +0000

    initial release

---
+++ about.md
@@ -0,0 +1,18 @@
+---
+layout: page
+title: About
+permalink: /about/
+---
+Hey!  I'm Sean and this is my website and blog.
+
+I'm a computer science student in Boston with a strong interest in functional programming (particularly OCaml and Scala), web development, and concurrent programming.  You'll find me on [github][github] and [twitter][twitter].
+
+I've becoming increasingly interested in web development, splitting my time between ruby on rails, play, nodejs, and angularjs.
+
+Feel free to send me an email at sdwalsh [at] mirango.io and consider using my [pgp key][pgpkey].
+
+Mirango.io content is served using Heroku!
+
+[twitter]: https://www.twitter.com/mirangomadness
+[github]: https://github.com/sdwalsh
+[pgpkey]: https://keybase.io/sdwalsh
---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+    
+    $("#mirango").typed({
+        strings: ["mirango.io"],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 250,
+        // time before typing starts
+        startDelay: 0,
+        // backspacing speed
+        backSpeed: 0,
+        // shuffle the strings
+        shuffle: false,
+        // time before backspacing
+        backDelay: 500,
+        // loop
+        loop: false,
+        // false = infinite
+        loopCount: false,
+        // show cursor
+        showCursor: true,
+        // character for cursor
+        cursorChar: "_",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'html',
+        // call when done callback function
+        callback: function() {},
+        // starting callback function before each string
+        preStringTyped: function() {},
+        //callback for every typed string
+        onStringTyped: function() {},
+        // callback for reset
+        resetCallback: function() {}
+    });
+});
